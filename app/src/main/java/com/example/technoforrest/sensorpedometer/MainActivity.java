7dd94000cfd675f7b6ac9bf61c8a4351fa347f14
technoforrest <31766195+technoforrest@users.noreply.github.com>
2017-11-25 12:47:57 -0800

    OAuth2.0Client added
    Not sure if client is asking permissions

--- app/src/main/java/com/example/technoforrest/sensorpedometer/MainActivity.java
+++ app/src/main/java/com/example/technoforrest/sensorpedometer/MainActivity.java
@@ -1,63 +1,60 @@
 package com.example.technoforrest.sensorpedometer;
 
-import android.app.Activity;
+
 import android.content.Context;
-import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.request.DataReadRequest;
-import com.google.android.gms.fitness.result.DataReadResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private Sensor sensor;
+    private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1;
     private SensorManager manager;
     private TextView count;
-    static final String TAG = "MainActivity";
-
     private Boolean activityRunning;
+    static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Log.d(TAG, "onCreate: ");
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        count = (TextView) findViewById(R.id.stepsText);
         manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-
+        count = findViewById(R.id.stepsText);
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .build();
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+            Log.d(TAG, "onCreate: NO PERMISSIONS IF STATEMENT");
+        } else {
+            Log.d(TAG, "onCreate: HAS PERMISSIONS ELSE STATEMENT");
+            accessGoogleFit();
+        }
     }
-
     /**
      * determines if the app is running
      */
@@ -94,6 +91,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    private void accessGoogleFit() {
+        //Does GoogleFit use the calendar to store steps???
+        Log.d(TAG, "accessGoogleFit: ");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+
+        /*DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();*/
+
 
     }
 }
