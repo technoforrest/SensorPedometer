98d2c31dd1cceaa2cb158d8c4826f7db389b51e4
technoforrest <31766195+technoforrest@users.noreply.github.com>
2017-11-28 19:38:30 -0800

    steps zero out at launch

--- app/src/main/java/com/example/technoforrest/sensorpedometer/MainActivity.java
+++ app/src/main/java/com/example/technoforrest/sensorpedometer/MainActivity.java
@@ -7,28 +7,23 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.fitness.FitnessOptions;
-import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.request.DataReadRequest;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private SensorManager manager;
     private TextView count;
-    private Boolean activityRunning;
     static final String TAG = "MainActivity";
+    private float steps;
+    private Boolean zeroSteps;
+    private float stepsDontCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,75 +34,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setSupportActionBar(toolbar);
         manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         count = findViewById(R.id.stepsText);
-        FitnessOptions fitnessOptions = FitnessOptions.builder()
-                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .build();
-        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
-            GoogleSignIn.requestPermissions(
-                    this, // your activity
-                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
-                    GoogleSignIn.getLastSignedInAccount(this),
-                    fitnessOptions);
-            Log.d(TAG, "onCreate: NO PERMISSIONS IF STATEMENT");
-        } else {
-            Log.d(TAG, "onCreate: HAS PERMISSIONS ELSE STATEMENT");
-            accessGoogleFit();
-        }
-    }
-    /**
-     * determines if the app is running
-     */
-    public void onResume(){
-        super.onResume();
-        activityRunning = true;
-        Sensor countSensor = manager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        if(countSensor != null){
-            manager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
-        }else{
-            Toast.makeText(this, "Count sensor error!", Toast.LENGTH_LONG).show();
-        }
+        zeroSteps = true;
+        sensorRegister();
+
     }
 
-    /**
-     * determines if the app is no longer running
-     */
-    @Override
-    public void onPause(){
-        super.onPause();
-        activityRunning = false;
-    }
+    public void sensorRegister(){
+        manager.registerListener(new SensorEventListener() {
 
-    /**
-     * updates the TextView when steps are increased
-     * @param event is the sensor that is receiving the steps
-     */
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if(activityRunning){
-            count.setText(String.valueOf(event.values[0]));
-        }
-    }
+          @Override
+          public void onSensorChanged(SensorEvent event) {
+              if(zeroSteps){
+                  stepsDontCount = event.values[0];
+                  zeroSteps = false;
+              }
+            steps = event.values[0] - stepsDontCount;//subtracts the values stored in the
+                                                // phone so only the steps taken since the app show
+            count.setText(steps + "");
+          }
+          @Override
+          public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+          }
+        }, manager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER),
+                SensorManager.SENSOR_DELAY_UI);
+  }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: ");
     }
-    private void accessGoogleFit() {
-        //Does GoogleFit use the calendar to store steps???
-        Log.d(TAG, "accessGoogleFit: ");
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        long endTime = cal.getTimeInMillis();
-        cal.add(Calendar.YEAR, -1);
-        long startTime = cal.getTimeInMillis();
 
-
-        /*DataReadRequest readRequest = new DataReadRequest.Builder()
-                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
-                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
-                .build();*/
-
-
-    }
 }
