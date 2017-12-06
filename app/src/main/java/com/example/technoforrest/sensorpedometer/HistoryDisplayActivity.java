27f298464c72c2dfa25f561bfbf1b2eedd7601f5
technoforrest <31766195+technoforrest@users.noreply.github.com>
2017-12-05 21:53:57 -0800

    added up button, graph label issues
    Graph labels remove graph content, so not labels for graph, but content is showing

--- app/src/main/java/com/example/technoforrest/sensorpedometer/HistoryDisplayActivity.java
+++ app/src/main/java/com/example/technoforrest/sensorpedometer/HistoryDisplayActivity.java
@@ -7,19 +7,20 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.RequiresApi;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Calendar;
-
+import java.util.Date;
 
 
 //label graph
-//fix navigation between activities
 //remove shared preferences older than 7 days
 
 public class HistoryDisplayActivity extends AppCompatActivity {
@@ -32,6 +33,8 @@ public class HistoryDisplayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history_display);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
         getGraph();
         Intent intent = getIntent();
 
@@ -46,8 +49,8 @@ public class HistoryDisplayActivity extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar cal = Calendar.getInstance();
         String dateArr[] = new String[7];
-                // get starting date
-        cal.add(Calendar.DAY_OF_YEAR, -6);
+        // get starting date
+        cal.add(Calendar.DAY_OF_YEAR, -7);
 
         // loop adding one day in each iteration
         for(int i = 0; i< 7; i++){
@@ -56,27 +59,46 @@ public class HistoryDisplayActivity extends AppCompatActivity {
             Log.d(TAG, "getLast7Dates: " + sdf.format(cal.getTime()));
         }
         Log.d(TAG, "getGraph: " + dateArr[6]);
-        int day1 = getPreferences(dateArr[0]);
-        int day2 = getPreferences(dateArr[1]);
-        int day3 = getPreferences(dateArr[2]);
-        int day4 = getPreferences(dateArr[3]);
-        int day5 = getPreferences(dateArr[4]);
-        int day6 = getPreferences(dateArr[5]);
-        int day7 = getPreferences(dateArr[6]);
 
-        Log.d(TAG, "getGraph: " + day6);
+        Date d1 = cal.getTime();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        Date d2 = cal.getTime();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        Date d3 = cal.getTime();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        Date d4 = cal.getTime();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        Date d5 = cal.getTime();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        Date d6 = cal.getTime();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        Date d7 = cal.getTime();
+
+
         GraphView graph = (GraphView) findViewById(R.id.graph);
         //how to get steps for the last 7 days by date?
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, day1),//label date and steps by date
-                new DataPoint(1, day2),
-                new DataPoint(2, day3),
-                new DataPoint(3, day4),
-                new DataPoint(4, day5),
-                new DataPoint(5, day6),
-                new DataPoint(6, day7)
+                new DataPoint(0, getPreferences(dateArr[0])),//label date and steps by date
+                new DataPoint(1, getPreferences(dateArr[1])),
+                new DataPoint(2, getPreferences(dateArr[2])),
+                new DataPoint(3, getPreferences(dateArr[3])),
+                new DataPoint(4, getPreferences(dateArr[4])),
+                new DataPoint(5, getPreferences(dateArr[5])),
+                new DataPoint(6, getPreferences(dateArr[6]))
         });
         graph.addSeries(series);
+        // set date label formatter
+        //graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+        //graph.getGridLabelRenderer().setNumHorizontalLabels(7); // only 4 because of the space
+
+// set manual x bounds to have nice steps
+        //graph.getViewport().setMinX(d7.getTime());
+        //graph.getViewport().setMaxX(d1.getTime());
+        //graph.getViewport().setXAxisBoundsManual(true);
+
+// as we use dates as labels, the human rounding to nice readable numbers
+// is not necessary
+        //graph.getGridLabelRenderer().setHumanRounding(false);
     }
 
     /**
