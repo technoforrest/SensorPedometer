f580febcbd77e53495c66fed88f932687e043359
technoforrest <31766195+technoforrest@users.noreply.github.com>
2017-12-06 14:26:49 -0800

    labels and graph updated

--- app/src/main/java/com/example/technoforrest/sensorpedometer/HistoryDisplayActivity.java
+++ app/src/main/java/com/example/technoforrest/sensorpedometer/HistoryDisplayActivity.java
@@ -49,56 +49,48 @@ public class HistoryDisplayActivity extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar cal = Calendar.getInstance();
         String dateArr[] = new String[7];
+        Date arrDate[] = new Date[7];
         // get starting date
-        cal.add(Calendar.DAY_OF_YEAR, -7);
+        cal.add(Calendar.DATE, -7);
 
         // loop adding one day in each iteration
         for(int i = 0; i< 7; i++){
-            cal.add(Calendar.DAY_OF_YEAR, 1);
-            dateArr[i] = sdf.format(cal.getTime());
-            Log.d(TAG, "getLast7Dates: " + sdf.format(cal.getTime()));
+            cal.add(Calendar.DATE, 1);
+            dateArr[i] = sdf.format(cal.getTime());//key for shared preferences
+            arrDate[i] = cal.getTime();
+
+            Log.d(TAG, "getLast7Dates: " + sdf.format(cal.getTime()) + " " + arrDate[i]);
         }
         Log.d(TAG, "getGraph: " + dateArr[6]);
-
-        Date d1 = cal.getTime();
-        cal.add(Calendar.DAY_OF_YEAR, -1);
-        Date d2 = cal.getTime();
-        cal.add(Calendar.DAY_OF_YEAR, -1);
-        Date d3 = cal.getTime();
-        cal.add(Calendar.DAY_OF_YEAR, -1);
-        Date d4 = cal.getTime();
-        cal.add(Calendar.DAY_OF_YEAR, -1);
-        Date d5 = cal.getTime();
-        cal.add(Calendar.DAY_OF_YEAR, -1);
-        Date d6 = cal.getTime();
-        cal.add(Calendar.DAY_OF_YEAR, -1);
-        Date d7 = cal.getTime();
 
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
         //how to get steps for the last 7 days by date?
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, getPreferences(dateArr[0])),//label date and steps by date
-                new DataPoint(1, getPreferences(dateArr[1])),
-                new DataPoint(2, getPreferences(dateArr[2])),
-                new DataPoint(3, getPreferences(dateArr[3])),
-                new DataPoint(4, getPreferences(dateArr[4])),
-                new DataPoint(5, getPreferences(dateArr[5])),
-                new DataPoint(6, getPreferences(dateArr[6]))
+                new DataPoint(arrDate[0], getPreferences(dateArr[0])),//label date and steps by date
+                new DataPoint(arrDate[1], getPreferences(dateArr[1])),
+                new DataPoint(arrDate[2], getPreferences(dateArr[2])),
+                new DataPoint(arrDate[3], getPreferences(dateArr[3])),
+                new DataPoint(arrDate[4], getPreferences(dateArr[4])),
+                new DataPoint(arrDate[5], getPreferences(dateArr[5])),
+                new DataPoint(arrDate[6], getPreferences(dateArr[6]))
         });
         graph.addSeries(series);
+        Log.d(TAG, "getGraph: " + series);
         // set date label formatter
-        //graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
-        //graph.getGridLabelRenderer().setNumHorizontalLabels(7); // only 4 because of the space
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(7); // only 4 because of the space
+        graph.getGridLabelRenderer().setHorizontalLabelsAngle(135);
 
 // set manual x bounds to have nice steps
-        //graph.getViewport().setMinX(d7.getTime());
-        //graph.getViewport().setMaxX(d1.getTime());
-        //graph.getViewport().setXAxisBoundsManual(true);
+        Log.d(TAG, "getGraph: " );
+        graph.getViewport().setMinX(arrDate[0].getTime());
+        graph.getViewport().setMaxX(arrDate[6].getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
 
 // as we use dates as labels, the human rounding to nice readable numbers
 // is not necessary
-        //graph.getGridLabelRenderer().setHumanRounding(false);
+        graph.getGridLabelRenderer().setHumanRounding(false);
     }
 
     /**
@@ -107,7 +99,7 @@ public class HistoryDisplayActivity extends AppCompatActivity {
      * @return the number of steps taken on the specific day accessed
      */
     public int getPreferences(String key){
-        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         return sharedPreferences.getInt(key, 0);
     }
 
@@ -119,6 +111,16 @@ public class HistoryDisplayActivity extends AppCompatActivity {
         SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
         Log.d(TAG, "dateTime: " + date.format(Calendar.getInstance().getTime()));
         return date.format(Calendar.getInstance().getTime());
+    }
+
+    /**
+     * Removes shared preferences by date
+     * @param key shared preference key is the date is was created
+     */
+    public void removePreferences(String key){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        sharedPreferences.edit().remove(key).apply();
+
     }
 
 
